da0500ba241937bd4a660e78de27ecc1b724ad9b
Deivison Oliveira <deivisonoliveira.info@gmail.com>
2019-01-14 22:19:45 -0400

    refatoração na ServletRelatorio

--- src/br/com/relatorio/model/ServletRelatorio.java
+++ src/br/com/relatorio/model/ServletRelatorio.java
@@ -23,32 +23,44 @@ import br.com.relatorio.util.ConnectionFactory;
 public class ServletRelatorio extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
 		try {
 			Connection conexao = new ConnectionFactory().getConnection();
-			
+
 			String paramDataInicio = req.getParameter("data_ini");
 			String paramDataFim = req.getParameter("data_fim");
-			
-			SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-			Date dataInicio = format.parse(paramDataInicio);
-			Date dataFim = format.parse(paramDataFim);
-			
+
+			Date dataInicio = converteStringParaData(paramDataInicio);
+			Date dataFim = converteStringParaData(paramDataFim);
+
 			Map<String, Object> parametros = new HashMap<>();
 			parametros.put("DATA_INI", dataInicio);
 			parametros.put("DATA_FIM", dataFim);
-			
+
 			String nomeArquivo = req.getRealPath("/WEB-INF/jasper/relatorio_movimentacoes.jasper");
 			GeradorDeRelatorios geradorDeRelatorios = new GeradorDeRelatorios(nomeArquivo, conexao, parametros);
-			String diretorio   = "pdf/";
 			OutputStream arquivo = resp.getOutputStream();
+			
 			geradorDeRelatorios.geraRelatorios(arquivo);
-		} catch (ParseException e) {
+			
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		
+
+	}
+
+	private Date converteStringParaData(String data) {
+		Date novaData = new Date();
+		if(data != null) {
+			SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+			try {
+				novaData = format.parse(data);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+		}
+		return novaData;
 	}
 }
